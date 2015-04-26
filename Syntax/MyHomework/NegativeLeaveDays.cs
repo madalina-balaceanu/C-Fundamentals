cf614369e163053673292ff1c9a74b45e82bb3d8
madalina.balaceanu <madalina.balaceanu93@gmail.com>
2015-04-26 22:42:52 +0300

    NegativeLeaveDays

---
+++ Syntax/MyHomework/NegativeLeaveDays.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class NegativeLeaveDays : Exception
+    {
+        public NegativeLeaveDays(string message):
+            base(message)
+        {
+
+        }
+    }
+}
